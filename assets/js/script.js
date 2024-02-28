61fc43331bb533d8aa423087dd6d606c8fb70ffd
Xelba91 <a_lbert_o@hotmail.com>
2024-02-28 17:06:00 +0100

    carrello

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,71 @@ document.addEventListener("DOMContentLoaded", function () {
   // Seleziona l'elemento corretto
   const container = document.querySelector("#book-container");
 
+  //   localStorage.getItem("carrello"): Questo metodo di localStorage viene utilizzato per recuperare il valore associato alla chiave "carrello". Se non è presente nessun valore associato a questa chiave, restituirà null.
+  //   JSON.parse(...): Questo metodo viene utilizzato per convertire una stringa JSON in un oggetto JavaScript. Poiché localStorage memorizza solo dati sotto forma di stringhe, dobbiamo utilizzare JSON.parse per convertire il valore restituito da localStorage.getItem("carrello") in un oggetto JavaScript.
+  //   || []: Questa è una tecnica nota come "short-circuit evaluation" in JavaScript. Se il valore recuperato da localStorage è null (quindi se non è stato trovato alcun dato associato alla chiave "carrello"), l'espressione || [] restituirà un array vuoto ([]). In questo modo, garantiamo che carrello contenga sempre un array, anche se non ci sono dati salvati in localStorage.
+  //   Quindi, la variabile carrello conterrà i dati del carrello recuperati da localStorage, se presenti, altrimenti conterrà un array vuoto. Questo approccio è molto comune nell'utilizzo di localStorage per memorizzare dati localmente nel browser.
+
+  let carrello = JSON.parse(localStorage.getItem("carrello")) || [];
+
+  // Funzione per aggiungere un libro al carrello
+  function aggiungiAlCarrello(libro) {
+    carrello.push(libro);
+    // Salva il carrello nel localStorage
+    localStorage.setItem("carrello", JSON.stringify(carrello));
+
+    // Aggiorna il carrello
+    const listCarr = document.getElementById("lista-carrello");
+    const carrElem = document.createElement("li");
+    carrElem.className = "list-group-item d-flex justify-content-between align-items-center my-1 rounded-2 ";
+    carrElem.textContent = `${libro.title} - ${libro.price}€`;
+
+    // tasto rimuovi nel carrello
+    const btnRemove = document.createElement("button");
+    btnRemove.className = "btn btn-danger btn-sm px-3 py-1";
+    btnRemove.textContent = "Rimuovi";
+    btnRemove.onclick = function () {
+      rimuoviDalCarrello(libro, carrElem);
+    };
+
+    carrElem.appendChild(btnRemove);
+    listCarr.appendChild(carrElem);
+  }
+
+  function funClear() {
+    carrello.length = 0;
+    localStorage.setItem("carrello", JSON.stringify(carrello));
+    document.getElementById("lista-carrello").innerHTML = "";
+  }
+
+  // Creo un div che fungerà da contenitore per il pulsante
+  const divContainer = document.createElement("div");
+  divContainer.className = "text-center mt-3"; // Aggiungi le classi per centraggio e margin-top
+
+  // Creo il btn per svuotare il carrello
+  const btnSvuota = document.createElement("button");
+  btnSvuota.className = "btn btn-warning btn-sm fw-bold px-3 py-1";
+  btnSvuota.textContent = "Svuota Carrello";
+  btnSvuota.onclick = funClear;
+
+  // Aggiungo il btn al div contenitore
+  divContainer.appendChild(btnSvuota);
+
+  // Aggiungo il div contenitore sotto la lista del carrello
+  document.getElementById("carrello").appendChild(divContainer);
+
+  // rimuovere dal carrello il libro
+  function rimuoviDalCarrello(libroDaRimuovere, elementoCarrello) {
+    // Rimuove il libro dall'array carrello
+    const indice = carrello.indexOf(libroDaRimuovere);
+    if (indice > -1) {
+      carrello.splice(indice, 1);
+    }
+    localStorage.setItem("carrello", JSON.stringify(carrello));
+    elementoCarrello.remove();
+  }
+
+  //funzione per creare la card che viene richiamata dal then una volta ricevuto il dato completo
   function creaCard(libro) {
     const col = document.createElement("div"); // creo un div con classe
     col.className = "col-12 col-md-6 col-lg-4 my-4";
@@ -49,6 +114,18 @@ document.addEventListener("DOMContentLoaded", function () {
     card.appendChild(cardBody);
     col.appendChild(card);
 
+    // <div class="col-12 col-md-6 col-lg-4 my-4">               div col
+    //     <div class="card h-100" style="max-height: 450px;">   div card
+    //         <img src="..." alt="... ">
+    //             <div class="card-body d-flex flex-column justify-content-between align-items-center">                                         div cardBody
+    //                 <h5 class="card-title">...</h5
+    //                 <p class="card-text">...</p>
+    //                 <button class="btn btn-danger px-3 py-1">Scarta</button>
+    //                 <button class="btn btn-success mx-2 px-3 py-1">Compra ora</button>
+    //                 </div>
+    //                 </div>
+    //                 </div>
+
     return col;
   }
 
@@ -56,7 +133,8 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((response) => {
       if (response.ok) {
-        return response.json(); // Continua solo se la risposta è valida
+        // Continua solo se la risposta è valida
+        return response.json();
       } else {
         console.error("Problemi di rete.");
         return []; // Ritorna un array vuoto in caso di risposta non valida
@@ -65,6 +143,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((libri) => {
       libri.forEach((libro) => {
         const card = creaCard(libro);
+        // div id book-container
         container.appendChild(card);
       });
     })
